5690201028c3bcdd883cc9bd049b20b66ec3f1bb
Adam Labadorf <alabadorf@gmail.com>
2011-06-13 20:01:48 -0400

    added scripts, arbitrarily increased version subnumber

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+scripts = ['scripts/getopts.py',
+           'scripts/steplist.py',
+           'scripts/parse_steplist.py'
+          ]
+
 setup(name='pypeline',
-		version='1.1',
-		py_modules=['pypeline'],
-		)
+      version='1.2',
+      py_modules=['pypeline'],
+      scripts=scripts,
+    )
